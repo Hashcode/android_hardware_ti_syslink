f0794aaafd758867840caa0207f0d7e898c728f5
Hashcode <hashcode0f@gmail.com>
2012-01-26 02:11:31 -0800

    re-adding ipc_listener

---
+++ ipc-listener/Android.mk
@@ -0,0 +1,17 @@
+#TODO add license
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_PRELINK_MODULE := false
+LOCAL_SRC_FILES := \
+	ISyslinkIpcListener.cpp \
+	SyslinkIpcListener.cpp
+
+LOCAL_SHARED_LIBRARIES:= \
+    libutils \
+    libbinder
+
+LOCAL_MODULE := libsyslink_ipc_listener
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
